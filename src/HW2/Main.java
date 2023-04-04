cc8fa15bb050ca4a4e6100eccb613cab728e0ef9
Konon <alenikon@mail.ru>
2023-04-04 16:51:31 +0300

    Initial commit

---
+++ src/HW2/Main.java
@@ -0,0 +1,12 @@
+package HW2;
+import HW2.task.*;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int[] arr = new int[]{3,2,1,5,4,51,8,7};
+        Task1.writeIteration(arr);
+        Task2.ParseString();
+    }
+}
